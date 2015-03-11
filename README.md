69e5b9acd4b05c6f06938ec5d9d4f58ef84a2f5b
Sergio Rus <sergio@sergiorus.com>
2015-03-11 17:16:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Homepage
+
+Sergio Rus personal website.
+
+## System requirements
+
+- Python 2.7.3
+- Pip 1.5.6
+- Virtualenv 1.11.6
+- Virtualenvwrapper 4.2
+- Ruby 2.2.0
+- Node.js 0.10.33
+
+## Project deployment
+
+Move to the same path as this file and run: `./bin/build.sh`
---
+++ bin/build.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# Current path
+proj_path="`pwd`"
+
+# Project name
+proj_name="`basename $proj_path`"
+
+# Ruby version
+ruby_version="`grep '^ruby ' Gemfile | awk '{print $2}' | cut -d"'" -f2`"
+
+# Node version
+node_version="`grep '"node":' package.json | awk '{print $2}' | tr -d '"<>='`"
+
+# Prompt colors
+txtrst='\e[0m'     # text reset
+txtylw='\e[0;33m'  # regular yellow
+txtgrn='\e[0;32m'  # regular green
+bldgrn='\e[1;32m'  # bold green
+
+# User prompt
+echo ""
+echo -en "${txtylw}Name of project's virtualenv: ${txtrst}"
+read venv_name
+echo ""
+
+# Check if dir exists
+if [[ ! -d "${proj_path}/conf" ]]; then
+    mkdir "${proj_path}/conf" || { echo "FATAL: Could not create 'conf' directory"; exit 1; }
+fi
+
+# Build virtualenv environment
+source /usr/local/bin/virtualenvwrapper.sh || { echo "FATAL: Could not find '/usr/local/bin/virtualenvwrapper.sh'"; exit 1; }
+mkvirtualenv "$venv_name"
+
+# Set and activate virtualenv postactivate script
+cd "${proj_path}/boot/virtualenv/bin/"
+cp postactivate.skel postactivate
+cp postdeactivate.skel postdeactivate
+path="${proj_path//\//\\/}"  # escape forward slashes
+sed -i "s/{{proj_path}}/$path/g" postactivate
+sed -i "s/{{node_version}}/$node_version/g" postactivate
+sed -i "s/{{ruby_version}}/$ruby_version/g" postactivate
+mv -f postactivate "${WORKON_HOME}/${venv_name}/bin/postactivate"
+mv -f postdeactivate "${WORKON_HOME}/${venv_name}/bin/postdeactivate"
+deactivate
+source /usr/local/bin/virtualenvwrapper.sh || { echo "FATAL: Could not find '/usr/local/bin/virtualenvwrapper.sh'"; exit 1; }
+workon "$venv_name"
+
+echo ""
+echo -e "${txtgrn}Installing Python packages...${txtrst}"
+echo ""
+
+# Install Python packages
+pip install -U -r "${proj_path}/requirements/development.txt" || { echo "FATAL: Could not install Python packages"; exit 1; }
+
+# Create link to Python packages
+ln -sf "$WORKON_HOME/${venv_name}/lib/python2.7/site-packages" "${proj_path}/python_packages"
+
+# Install and set Ruby virtual environment: http://rvm.io/
+curl -sSL https://get.rvm.io | bash || { echo "FATAL: Could not install RVM"; exit 1; }
+source "$HOME/.rvm/scripts/rvm"
+rvm install $ruby_version
+rvm use $ruby_version@$venv_name --create
+
+echo ""
+echo -e "${txtgrn}Installing Ruby packages...${txtrst}"
+echo ""
+
+# Install Ruby packages
+bundle install || { echo "FATAL: Could not install Ruby packages"; exit 1; }
+
+# Install and set Node.js virtual environment: https://github.com/creationix/nvm
+curl https://raw.githubusercontent.com/creationix/nvm/v0.22.1/install.sh | bash
+source ~/.nvm/nvm.sh || { echo "FATAL: Could not install NVM"; exit 1; }
+nvm install $node_version
+
+# Install the latest NPM
+curl -L https://npmjs.com/install.sh | sh || { echo "FATAL: Could not install NPM"; exit 1; }
+
+echo ""
+echo -e "${txtgrn}Installing Node packages from root project...${txtrst}"
+echo ""
+
+# Install Node dependencies from root project
+npm install || { echo "FATAL: Could not install root project Node packages"; exit 1; }
+
+echo ""
+echo -e "${txtgrn}Installing Node packages from frontend...${txtrst}"
+echo ""
+
+# Install Node dependencies from frontend
+cd "${proj_path}/src/frontend/${proj_name}"
+npm install || { echo "FATAL: Could not install frontend Node packages"; exit 1; }
+cd "$proj_path"
+
+echo ""
+echo -e "${txtgrn}Configuring IPython...${txtrst}"
+echo ""
+
+# Set IPython default profile
+ipython profile create || { echo "FATAL: Could not create IPython profile"; exit 1; }
+ln -sf "${proj_path}/boot/ipython/ipython_config.py" "${proj_path}/conf/ipython/profile_default/ipython_config.py"
+
+echo ""
+echo -e "${bldgrn}OK, project deployed.${txtrst}"
+echo ""
+
+exit 0
