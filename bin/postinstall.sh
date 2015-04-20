bd94e8c7d5bf404a2e3573ac912b91e25f3297f2
Sergio Rus <sergio@sergiorus.com>
2015-04-20 17:21:39 +0200

    Remove prompt message

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Prompt colors
-txtrst='\e[0m'     # text reset
-txtylw='\e[0;33m'  # regular yellow
-txtgrn='\e[0;32m'  # regular green
-bldgrn='\e[1;32m'  # bold green
-
 # Current path
 proj_path="`pwd`"
 
@@ -13,9 +7,5 @@ proj_path="`pwd`"
 cd "${proj_path}/src/frontend/homepage"
 npm install || { echo "FATAL: Could not install Node packages from ${proj_path}/src/frontend/homepage"; exit 1; }
 cd "$proj_path"
-
-echo ""
-echo -e "${bldgrn}OK, project deployed.${txtrst}"
-echo ""
 
 exit 0
