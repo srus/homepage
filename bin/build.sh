2c6b97cd2872cd64e543cf1fd42390388f31b87e
Sergio Rus <sergio@sergiorus.com>
2015-04-21 12:55:32 +0200

    Use the right folder name for the front-end path

--- bin/build.sh
+++ bin/build.sh
@@ -96,8 +96,8 @@ echo -e "${txtgrn}Installing Node packages from front-end...${txtrst}"
 echo ""
 
 # Install Node packages from front-end
-cd "${proj_path}/src/frontend/${proj_name}"
-npm install || { echo "FATAL: Could not install Node packages from front-end"; exit 1; }
+cd "${proj_path}/src/frontend/homepage"
+npm install || { echo "FATAL: Could not install Node packages from ${proj_path}/src/frontend/homepage"; exit 1; }
 cd "$proj_path"
 
 echo ""
